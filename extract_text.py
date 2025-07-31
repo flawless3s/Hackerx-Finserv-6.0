20666d73296c33b96726c9463d2b836249510cf3
flawless3s <marcdaniel2506@gmail.com>
2025-07-31 20:49:47 +0530

    Word Doc Text extraction

--- extract_text.py
+++ extract_text.py
@@ -1,4 +1,5 @@
-import fitz  # PyMuPDF
+import fitz # PyMuPDF
+from docx import Document  
 
 def extract_text_from_pdf(path):
     try:
@@ -8,7 +9,23 @@ def extract_text_from_pdf(path):
         print(f"Error reading PDF: {e}")
         return ""
 
+def extract_text_from_docx(path):
+    try:
+        doc = Document(path)
+        full_text = []
+        for para in doc.paragraphs:
+            full_text.append(para.text)
+        return '\n'.join(full_text)
+    except Exception as e:
+        print(f"Error reading Word Doc: {e}")
+        return ""
+    
+
 if __name__ == "__main__":
     pdf_path = "sem-1.pdf"
     text = extract_text_from_pdf(pdf_path)
     print(text[:1000])
+    doc_path = "FINAL YEAR SYNOPSIS.docx"
+    text_doc = extract_text_from_docx(doc_path)
+    print(text_doc[:1000])
+    
